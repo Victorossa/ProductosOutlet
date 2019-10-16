c69fe1da8f65efd747a2f941746c775045a82b6b
Programador1 <programador1@comedal.com.co>
2019-10-16 09:55:50 -0500

    Agregado Componentes-Estilos-Servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productosppal/productos/productos.component';
+import { ProductoComponent } from './productosppal/producto/producto.component';
+import { ProductoListComponent } from './productosppal/producto-list/producto-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    ProductoComponent,
+    ProductoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/productosppal/producto-list/producto-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-list',
+  templateUrl: './producto-list.component.html',
+  styleUrls: ['./producto-list.component.css']
+})
+export class ProductoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
